39eb6067f34bf3d026cb9d29122b67b6792a5dbd
uğur cengiz <ugur_nobre@hotmail.com>
2022-12-19 18:02:53 +0300

    19.12.22

---
+++ src/app/admin/components/products/create/create.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+
+
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Create_Product } from 'src/app/contracts/create_product';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/models/product.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent extends BaseComponent implements OnInit {
+
+  constructor(spinner:NgxSpinnerService,private productService:ProductService,private alertify:AlertifyService) {
+    super(spinner)
+   }
+
+  ngOnInit(): void {
+
+    
+  }
+
+  create(name:HTMLInputElement,stock:HTMLInputElement,price:HTMLInputElement){
+    this.showSpinner(SpinnerType.ballatom)
+      const create_product :Create_Product= new Create_Product();
+      create_product.name= name.value;
+      create_product.stock=parseInt(stock.value);
+      create_product.price= parseFloat(price.value);     
+
+      if(!name.value.length == null){
+        this.alertify.message("Lütfen ürün adını giriniz!",{
+          dismissOthers:true,
+          messageType:MessageType.Error,
+          position:Position.BottomRight
+        });
+        return;
+      }
+
+      if(parseInt(stock.value)<0){
+        this.alertify.message("Lütfen stok bilgisini doğru giriniz!",{
+          dismissOthers:true,
+          messageType:MessageType.Error,
+          position:Position.BottomRight
+        });
+        return;
+      }
+
+
+
+      this.productService.create(create_product,()=>{
+        this.hideSpinner(SpinnerType.ballatom);
+        
+        this.alertify.message("Ürün başarıyla eklenmiştir",{
+          dismissOthers:true,
+          messageType:MessageType.Success,
+          position:Position.TopRight
+        });
+      },errorMessage=>{
+        this.alertify.message(errorMessage,{
+          dismissOthers:true,
+          messageType:MessageType.Error,
+          position:Position.TopRight
+        })
+      });
+  }
+
+}
--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
+
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -16,38 +16,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.ballpulsesync)
-
-    this.httpClientService.get<Product[]>({
-      controller:"products"
-    }).subscribe(data=>console.log(data));
-
-
-    //  this.httpClientService.post({
-    //    controller: "products"
-    //  }, {
-    //    name: "Kalem",
-    //   stock: 100,
-    //    price: 15
-    //  }).subscribe();
-
-
-
     
-    // this.httpClientService.put({
-    //   controller:"products"
-    // },{
-    //   id: "97a47897-05d8-4c4b-afa2-a763ac2da5f0",
-    //   name:"kağıt",
-    //   stock:500,
-    //   price:12
-    // }).subscribe();
-
-    // this.httpClientService.delete({
-    //   controller: "products"
-    // },"36b308b1-704c-491e-b450-10699eb3c970").subscribe();
-
-
 
     
   }
